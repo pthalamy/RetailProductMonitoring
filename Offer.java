efcd590959f32de4832266fca885156c0aad403a
Pierre Thalamy <pierre.thalamy@gmail.com>
2016-11-14 18:00:36 +0100

    ToString implementation, offerPriceFromString skeleton, update constructor

--- Offer.java
+++ Offer.java
@@ -5,12 +5,31 @@ public class Offer {
 	public String companyUrl;
 	public Double price;
 
-	public Offer(String companyName, Country country, String companyUrl, Double price) {
+	public Offer(String companyName, String companyUrl, Country country, String priceStr) {
 		super();
 
 		this.companyName = companyName;
 		this.country = country;
 		this.companyUrl = companyUrl;
-		this.price = price;
+		this.price = offerPriceFromString(priceStr);
 	}
+
+	//! TODO
+	public static Double offerPriceFromString(String priceStr) {
+		return 0.0;
+	}
+
+	@Override
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		sb.append("\tOffer\t{\n");
+		sb.append("\t\tname: " + companyName + "\n");
+		sb.append("\t\turl: " + companyUrl + "\n");
+		sb.append("\t\tcountry: " + country + "\n");
+		sb.append("\t\tprice: " + price + "\n");
+		sb.append("\t}");
+		
+		return sb.toString();
+	}
+
 }
