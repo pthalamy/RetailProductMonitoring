c46e0323fc01a4f4cb484b8cd140fc2737508b46
Pierre Thalamy <pierre.thalamy@gmail.com>
2016-11-14 18:01:55 +0100

    Include offers in toString

--- Product.java
+++ Product.java
@@ -5,13 +5,14 @@ public class Product {
 	public String name;
 	public String ASIN;
 	public String url;
-	public ArrayList<Offer> vendors = new ArrayList<Offer>();
 	public Country country;
 	public Double retailPrice;
+	public ArrayList<Offer> offers = new ArrayList<Offer>();
 	
-	public Product(String ASIN, String url, Country country, Double price) {
+	public Product(String name, String ASIN, String url, Country country, Double price) {
 		super();
-		
+
+		this.name = name;
 		this.ASIN = ASIN;
 		this.url = url;
 		this.country = country;
@@ -21,13 +22,19 @@ public class Product {
 	@Override
 	public String toString() {
 		StringBuffer sb = new StringBuffer();
-		sb.append("PRODUCT " + ASIN + " {\n");
+		sb.append("Product " + ASIN + " {\n");
 		sb.append("\tname: " + name + "\n");
 		sb.append("\turl: " + url + "\n");
 		sb.append("\tcountry: " + country + "\n");
 		sb.append("\tprice: " + retailPrice + "\n");
-		sb.append("}");
-		
+		sb.append("\toffers: \n");
+
+		for (Offer offer : this.offers) {
+			sb.append(offer);
+		}
+
+		sb.append("\n}");
+
 		return sb.toString();
 	}
 }
